7d2ddc747d4e96ab9c8c6df7f1ec60695fa184ec
Sabrina Castro <81202040+cassabr@users.noreply.github.com>
2024-09-07 15:43:23 -0300

    Create 01.py

---
+++ S1/Introdução à Programação Estruturada/Unidade I/01.py
@@ -0,0 +1,7 @@
+# 1. 
+# Construa um programa que mostre o texto “Ola, mundo!” na tela. 
+# Dica: utilize uma função print() em um arquivo com nome ola.py 
+# Execute-o na linha de comando python ola.py
+
+import ola
+
