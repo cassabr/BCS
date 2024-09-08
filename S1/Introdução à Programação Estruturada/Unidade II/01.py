b6f7edd50712f981f412cdf11f18ed809f34f233
Sabrina Castro <81202040+cassabr@users.noreply.github.com>
2024-09-08 15:02:39 -0300

    Create 01.py

---
+++ S1/Introdução à Programação Estruturada/Unidade II/01.py
@@ -0,0 +1,10 @@
+# 1. 
+# Crie um programa que solicite um número inteiro ao usuário e imprima na tela uma mensagem dizendo se o número é positivo ou negativo.
+
+valor = int(input("Digite um valor inteiro: "))
+
+if valor >= 0:
+    print("Você digitou um valor positivo")
+
+else:
+    print("Você digitou um valor negativo")
